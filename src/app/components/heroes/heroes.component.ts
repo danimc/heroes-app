bb91f20b869e75e2efe6f1499ba15b0c55ca8e42
OAG HQ <daniel_k310a@hotmail.com>
2022-02-23 17:00:15 -0600

    añade vista de heroe invividual

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, HeroesService } from 'src/app/services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html'
+  templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
+  heroes: Heroe[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _heroesService: HeroesService, private router: Router) {
+    console.log('constructor');
   }
 
+  ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(idx: number) {
+   this.router.navigate( ['/heroe', idx] )
+  }
 }
