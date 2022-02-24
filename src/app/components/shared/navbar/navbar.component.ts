3f01291dd1eada70eb51e627d7934b679f1e7066
OAG HQ <daniel_k310a@hotmail.com>
2022-02-24 17:12:01 -0600

    app Terminada + componente de tarjetas

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor( private router:Router) {}
 
   ngOnInit(): void {}
 
   buscarHeroe(busqueda: string) {
     console.log(busqueda);
+
+    this.router.navigate(['/buscar', busqueda])
   }
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -62,15 +62,30 @@ export class HeroesService {
     return this.heroes;
   }
 
-  getHeroe(idx: number){
+  getHeroe(idx: number) {
     return this.heroes[idx];
+  }
+
+  buscarHeroes(termino: string): Heroe[] {
+    let heroesArr: Heroe[] = [];
+    termino = termino.toLowerCase();
+
+    for (let heroe of this.heroes) {
+      let nombre = heroe.nombre.toLowerCase();
+
+      if (nombre.indexOf(termino) >= 0) {
+        heroesArr.push(heroe);
+      }
+    }
+
+    return heroesArr;
   }
 }
 
- export interface Heroe {
+export interface Heroe {
   nombre: string;
   bio: string;
   img: string;
-  aparicion: string,
-  casa: string
+  aparicion: string;
+  casa: string;
 }
